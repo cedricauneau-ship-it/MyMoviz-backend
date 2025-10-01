80750bcabd93b9509deb0ac30b98103b64593d86
Cédric <cedricauneau@gmail.com>
2025-10-01 14:48:17 +0200

    version 1.0

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'https://my-moviz-backend-sepia.vercel.app/';
+const BACKEND_URL = 'https://my-moviz-backend-wine.vercel.app/';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = 'https://my-moviz-frontend-two-eta.vercel.app/';
+const FRONTEND_URL = 'https://my-moviz-frontend-mu-two.vercel.app/';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
